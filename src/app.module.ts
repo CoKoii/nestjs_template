932144cee1f3ec19ccf2a9ea20add8904173093a
CK. <2655257336@qq.com>
2026-01-30 16:35:48 +0800

    feat: 添加 JWT 认证守卫，重构权限控制，新增公共装饰器以支持公共路由

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Global, Logger, Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { APP_GUARD } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import * as dotenv from "dotenv";
 
 import { connectionParams, validationSchema } from "../ormconfig";
 import { LoggerModule } from "./common/logger/logger.module";
+import { JwtAuthGuard } from "./common/auth/jwt-auth.guard";
 import { AuthModule } from "./modules/auth/auth.module";
 import { UserModule } from "./modules/user/user.module";
 const envFilePath = `.env.${process.env.NODE_ENV || "development"}`;
@@ -23,7 +25,7 @@ const envFilePath = `.env.${process.env.NODE_ENV || "development"}`;
     AuthModule,
   ],
   controllers: [],
-  providers: [Logger],
+  providers: [Logger, { provide: APP_GUARD, useClass: JwtAuthGuard }],
   exports: [Logger],
 })
 export class AppModule {}
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from "@nestjs/common";
+import { Public } from "../../common/auth/public.decorator";
 import { AuthService } from "./auth.service";
 import { LoginUserDto } from "./dto/login-user.dto";
 import { RegisterUserDto } from "./dto/register-user.dto";
@@ -6,10 +7,12 @@ import { RegisterUserDto } from "./dto/register-user.dto";
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
+  @Public()
   @Post("/login")
   signin(@Body() loginUserDto: LoginUserDto) {
     return this.authService.signin(loginUserDto);
   }
+  @Public()
   @Post("/register")
   signup(@Body() registerUserDto: RegisterUserDto) {
     return this.authService.signup(registerUserDto);
