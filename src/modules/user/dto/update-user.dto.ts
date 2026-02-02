9569530241ef87ffc7b6232fc94d854fc1ee22c4
CK. <2655257336@qq.com>
2026-02-02 17:11:46 +0800

    feat: 更新 UpdateUserDto，添加角色ID字段及其验证

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, Length } from "class-validator";
+import { IsNumber, IsString, Length } from "class-validator";
 
 export class UpdateUserDto {
   @IsString()
@@ -8,4 +8,6 @@ export class UpdateUserDto {
   @Length(6, 20, { message: "密码长度应在$constraint1到$constraint2之间" })
   password: string;
   confirmPassword: string;
+  @IsNumber({}, { each: true, message: "角色ID必须为数字数组" })
+  roles: number[];
 }
