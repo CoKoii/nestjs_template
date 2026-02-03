bdf78df8f13be44c3fb4767163eaf115d2bba8d9
CK. <2655257336@qq.com>
2026-02-03 16:41:32 +0800

    feat: 添加argon2密码加密，优化用户注册和登录逻辑

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,12 +1,16 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+} from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { InjectRepository } from "@nestjs/typeorm";
+import * as argon2 from "argon2";
 import { Repository } from "typeorm";
 import { Profile } from "../profile/entities/profile.entity";
 import { User } from "../user/entities/user.entity";
 import { LoginUserDto } from "./dto/login-user.dto";
 import { RegisterUserDto } from "./dto/register-user.dto";
-
 @Injectable()
 export class AuthService {
   constructor(
@@ -17,25 +21,28 @@ export class AuthService {
   private signToken = (user: User) =>
     this.jwtService.signAsync({ sub: user.id, username: user.username });
   // ----------------------------------------------------------------------
-  //用户注册
+  //用户注册 密码加密
   async register(dto: RegisterUserDto) {
     if (dto.password !== dto.confirmPassword)
       throw new BadRequestException("两次输入的密码不一致");
     if (await this.users.findOneBy({ username: dto.username }))
       throw new BadRequestException("当前用户名已被注册");
     const user = await this.users.save(
-      this.users.create({ username: dto.username, password: dto.password }),
+      this.users.create({
+        username: dto.username,
+        password: await argon2.hash(dto.password),
+      }),
     );
     const profile = this.profiles.create({ nickname: dto.username, user });
     await this.profiles.save(profile);
     return { accessToken: await this.signToken(user) };
   }
   // ----------------------------------------------------------------------
-  //用户登录
+  //用户登录 密码验证
   async login(dto: LoginUserDto) {
     const user = await this.users.findOneBy({ username: dto.username });
-    if (!user || user.password !== dto.password)
-      throw new BadRequestException("用户名或密码错误");
+    if (!user || !(await argon2.verify(user.password, dto.password)))
+      throw new ForbiddenException("用户名或密码错误");
     return { accessToken: await this.signToken(user) };
   }
   // ----------------------------------------------------------------------
